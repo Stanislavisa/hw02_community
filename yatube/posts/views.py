0225ebb9fb086296ec903969d427472fe25bf1e8
Stanislav Izotov <sa.izotov@yandex.ru>
2022-08-14 21:13:22 +0400

    firs edit pep

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -4,9 +4,10 @@ from .models import Group, Post
 
 POST_PAGE = 10
 
+
 def index(request):
     posts = Post.objects.order_by("-pub_date")[:POST_PAGE]
-    
+
     context = {
         "posts": posts,
     }
@@ -18,10 +19,10 @@ def group_posts(request, slug):
     """Страница список постов."""
     group = get_object_or_404(Group, slug=slug)
     posts = Post.objects.filter(group=group).order_by("-pub_date")[:POST_PAGE]
-    
+
     context = {
         "group": group,
         "posts": posts,
     }
-    
+
     return render(request, "posts/group_list.html", context)
--- yatube/yatube/settings.py
+++ yatube/yatube/settings.py
@@ -9,8 +9,6 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
-
-
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
@@ -52,17 +50,12 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'yatube.wsgi.application'
 
-
-
-
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
-
-
 
 AUTH_PASSWORD_VALIDATORS = [
     {
@@ -79,9 +72,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
-
-
 LANGUAGE_CODE = 'en-us'
 
 TIME_ZONE = 'UTC'
@@ -91,8 +81,6 @@ USE_I18N = True
 USE_L10N = True
 
 USE_TZ = True
-
-
 
 STATIC_URL = '/static/'
 
