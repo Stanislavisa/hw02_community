b9c8055d966b4e138e4e6414e1c6b927d68acc9c
Stanislav Izotov <sa.izotov@yandex.ru>
2022-08-15 07:25:20 +0400

    4

--- yatube/posts/models.py
+++ yatube/posts/models.py
@@ -19,6 +19,6 @@ class Post(models.Model):
     text = models.TextField()
     pub_date = models.DateTimeField(auto_now_add=True)
     author = models.ForeignKey(User, on_delete=models.CASCADE,
-                               related_name="posts") 
+                               related_name="posts")
     group = models.ForeignKey(Group, on_delete=models.SET_NULL,
-                            blank=True, null=True)
+                               blank=True, null=True)
