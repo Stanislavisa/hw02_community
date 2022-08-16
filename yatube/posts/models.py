aa6bfb183fe58b756f4e50fa3d3dc5938e2f02d8
Stanislav Izotov <sa.izotov@yandex.ru>
2022-08-16 06:28:41 +0400

    после ревью

--- yatube/posts/models.py
+++ yatube/posts/models.py
@@ -18,11 +18,15 @@ class Post(models.Model):
     """Класс описывающий поля модели Post"""
     text = models.TextField()
     pub_date = models.DateTimeField(auto_now_add=True)
-    author = models.ForeignKey(User, on_delete=models.CASCADE,
-                               related_name="posts")
+    author = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name="posts"
+    )
     group = models.ForeignKey(
         Group,
         on_delete=models.SET_NULL,
         blank=True,
-        null=True
+        null=True,
+        related_name="posts"
     )
